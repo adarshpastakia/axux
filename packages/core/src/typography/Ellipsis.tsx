6db9b9cb42809c7123442ebc9616c63e3af9c52f
Adarsh Pastakia <adarshpastakia@outlook.com>
2022-04-28 16:32:30 +0400

    fix: ellipsis text component

--- packages/core/src/typography/Ellipsis.tsx
+++ packages/core/src/typography/Ellipsis.tsx
@@ -3,19 +3,43 @@
 // @copyright : 2022
 // @license   : MIT
 
-import { FC, useCallback, useState } from "react";
+import { FC, useCallback, useLayoutEffect, useRef, useState } from "react";
+import { debounce } from "@axux/utilities";
+import ResizeObserver from "resize-observer-polyfill";
 
 export const AxEllipsis: FC = ({ children }) => {
-  const [showEllipsis, setShowEllipsis] = useState(false);
-  const checkText = useCallback((el: HTMLElement) => {
-    setShowEllipsis(el && el.offsetWidth > (el.parentElement?.offsetWidth ?? 0));
+  const refSpan = useRef<HTMLSpanElement>(null);
+  const [showEllipsis, setShowEllipsis] = useState<string>();
+
+  const checkEllipsis = useCallback(() => {
+    if (refSpan.current) {
+      const el = refSpan.current;
+      const span = el.firstElementChild as HTMLElement;
+      setShowEllipsis(span.offsetWidth > el.offsetWidth + 8 ? span.innerText : undefined);
+    }
   }, []);
+
+  useLayoutEffect(() => {
+    setShowEllipsis(undefined);
+    debounce(checkEllipsis, 100)();
+    if (refSpan.current) {
+      const ob = new ResizeObserver(checkEllipsis);
+      ob.observe(refSpan.current);
+
+      return () => {
+        ob.disconnect();
+      };
+    }
+  }, [checkEllipsis, children]);
+
   return (
     <span
-      className={showEllipsis ? "ax-ellipsis--middle" : "ax-block"}
-      title={showEllipsis ? children?.toString() : undefined}
+      ref={refSpan}
+      title={showEllipsis}
+      data-enabled={!!showEllipsis}
+      className="ax-ellipsis--middle"
     >
-      <span ref={checkText}>{children}</span>
+      <span>{children}</span>
     </span>
   );
 };
