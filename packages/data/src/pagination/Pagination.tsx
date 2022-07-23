d659bd5d7d291dc10188d72befff91c2207877ea
Adarsh Pastakia <adarshpastakia@outlook.com>
2022-07-23 15:46:01 +0400

    fix(Pagination): added minimal display

--- packages/data/src/pagination/Pagination.tsx
+++ packages/data/src/pagination/Pagination.tsx
@@ -8,12 +8,13 @@
 
 import { AxIcon } from "@axux/core";
 import { AppIcons } from "@axux/core/dist/types/appIcons";
-import { FC } from "react";
+import { FC, Fragment, useMemo } from "react";
 
 export interface PaginationProps {
   page: number;
   totalPages: number;
   ranges: number[][];
+  isMinimal?: boolean;
   onPageChange: (page: number) => void;
 }
 
@@ -21,9 +22,48 @@ export const AxPagination: FC<PaginationProps> = ({
   page,
   ranges,
   totalPages,
+  isMinimal,
   onPageChange,
 }) => {
   const [start, mid, last] = ranges;
+
+  const pageBody = useMemo(
+    () => (
+      <Fragment>
+        {start?.map((pg) => (
+          <button
+            key={pg}
+            data-active={page === pg}
+            onClick={() => onPageChange(pg)}
+          >
+            {pg + 1}
+          </button>
+        ))}
+        {mid && <span>...</span>}
+        {mid?.map((pg) => (
+          <button
+            key={pg}
+            data-active={page === pg}
+            onClick={() => onPageChange(pg)}
+          >
+            {pg + 1}
+          </button>
+        ))}
+        {last && <span>...</span>}
+        {last?.map((pg) => (
+          <button
+            key={pg}
+            data-active={page === pg}
+            onClick={() => onPageChange(pg)}
+          >
+            {pg + 1}
+          </button>
+        ))}
+      </Fragment>
+    ),
+    [start, mid, last, page]
+  );
+
   return (
     <div className="ax-pagination">
       <button onClick={() => onPageChange(0)} data-disabled={page === 0}>
@@ -32,35 +72,12 @@ export const AxPagination: FC<PaginationProps> = ({
       <button onClick={() => onPageChange(page - 1)} data-disabled={page === 0}>
         <AxIcon icon={AppIcons.iconCaretLeft} rtlFlip />
       </button>
-      {start?.map((pg) => (
-        <button
-          key={pg}
-          data-active={page === pg}
-          onClick={() => onPageChange(pg)}
-        >
-          {pg + 1}
-        </button>
-      ))}
-      {mid && <span>...</span>}
-      {mid?.map((pg) => (
-        <button
-          key={pg}
-          data-active={page === pg}
-          onClick={() => onPageChange(pg)}
-        >
-          {pg + 1}
-        </button>
-      ))}
-      {last && <span>...</span>}
-      {last?.map((pg) => (
-        <button
-          key={pg}
-          data-active={page === pg}
-          onClick={() => onPageChange(pg)}
-        >
-          {pg + 1}
-        </button>
-      ))}
+      {!isMinimal && pageBody}
+      {isMinimal && (
+        <label>
+          {page + 1}/{totalPages}
+        </label>
+      )}
       <button
         onClick={() => onPageChange(page + 1)}
         data-disabled={page + 1 >= totalPages}
