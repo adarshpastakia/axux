99417b53ee7c2951e27fc474174fe0a2db3435b8
Adarsh Pastakia <adarshpastakia@outlook.com>
2022-08-13 01:21:57 +0400

    fix(getBoundingBox): update

--- packages/utilities/src/_boundingBox.ts
+++ packages/utilities/src/_boundingBox.ts
@@ -6,8 +6,11 @@
  * @license   : MIT
  */
 
-const _getBox = (box: AnyObject = "0,0,0,0") => {
+import { isEmpty } from "./_isType";
+
+const _getBox = (box: AnyObject) => {
   let boundingBox = box;
+  if (isEmpty(box)) return [];
   if (typeof box === "string") {
     boundingBox = box.split(",");
   }
@@ -19,28 +22,30 @@ const _getBox = (box: AnyObject = "0,0,0,0") => {
 
 /**
  * box "x,y,w,h" | [x,y,w,h]
- * returns canvas box
+ * returns canvas box [x,y,w,h]
  */
-export const getBoundingBox = (box: AnyObject = "0,0,0,0") => {
+export const getBox = (box: AnyObject = "0,0,0,0") => {
   const boundingBox = _getBox(box);
 
   let [x = 0, y = 0, w = 0, h = 0] = boundingBox;
+  w = x < 0 ? w + x : w;
+  h = y < 0 ? h + y : h;
   x = x > 0 ? x : 0;
   y = y > 0 ? y : 0;
-  w = w - x;
-  h = h - y;
   return [x, y, w, h] as [number, number, number, number];
 };
 
 /**
  * box "x1,y1,x2,y2" | [x1,y1,x2,y2]
- * returns canvas box
+ * returns canvas box [x,y,w,h]
  */
-export const getBox = (box: AnyObject = "0,0,0,0") => {
+export const getBoundingBox = (box: AnyObject = "0,0,0,0") => {
   const boundingBox = _getBox(box);
 
   let [x1 = 0, y1 = 0, x2 = 0, y2 = 0] = boundingBox;
+  const w = x2 - x1;
+  const h = y2 - y1;
   x1 = x1 > 0 ? x1 : 0;
   y1 = y1 > 0 ? y1 : 0;
-  return [x1, y1, x2, y2] as [number, number, number, number];
+  return [x1, y1, w, h] as [number, number, number, number];
 };
