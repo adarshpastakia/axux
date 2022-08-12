2557daa204b1e4fc39405d5f4050401e0d54c988
Adarsh Pastakia <adarshpastakia@outlook.com>
2022-08-12 23:10:19 +0400

    Revert "fix(Inputs): remove value state"
    This reverts commit 214a22e00b76695f95817bd87303538b13cbc955.

--- packages/form/src/inputs/Number.tsx
+++ packages/form/src/inputs/Number.tsx
@@ -9,7 +9,15 @@
 import { ChildrenProp, ElementProps } from "@axux/core/dist/types";
 import { isEmpty } from "@axux/utilities";
 import { handleEnter } from "@axux/utilities/dist/handlers";
-import { ChangeEvent, FC, memo, useCallback, useTransition } from "react";
+import {
+  ChangeEvent,
+  FC,
+  memo,
+  useCallback,
+  useEffect,
+  useState,
+  useTransition,
+} from "react";
 import { InputProps } from "../types";
 import { FieldWrapper } from "./Wrapper";
 
@@ -53,9 +61,14 @@ export const Number: FC<NumberProps> = memo(
     onEnterPressed,
     ...rest
   }) => {
+    const [actualValue, setActualValue] = useState<number>("" as AnyObject);
     const [pending, startTransition] = useTransition();
+    useEffect(() => {
+      setActualValue(value ?? ("" as AnyObject));
+    }, [value]);
     const handleChange = useCallback(
       (e?: ChangeEvent<HTMLInputElement>) => {
+        setActualValue(e?.target.valueAsNumber ?? ("" as AnyObject));
         onChange &&
           startTransition(() =>
             onChange(e?.target.valueAsNumber ?? ("" as AnyObject))
@@ -76,7 +89,7 @@ export const Number: FC<NumberProps> = memo(
         isInvalid={isInvalid}
         isRequired={isRequired}
         onClear={handleChange}
-        canClear={allowClear && !isEmpty(value)}
+        canClear={allowClear && !isEmpty(actualValue)}
       >
         <input
           ref={inputRef}
@@ -86,7 +99,7 @@ export const Number: FC<NumberProps> = memo(
           aria-readonly={isReadOnly}
           aria-required={isRequired}
           aria-errormessage={error}
-          value={value}
+          value={actualValue}
           placeholder={placeholder}
           disabled={isDisabled}
           readOnly={isReadOnly}
