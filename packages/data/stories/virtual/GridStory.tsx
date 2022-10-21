3ab0475a02dee58b706a078be09d8c00931dbea3
Adarsh Pastakia <adarshpastakia@outlook.com>
2022-10-21 14:29:58 +0400

    fix: virtual lists
    fix: virtual lists
    
    fix: virtual lists

--- packages/data/stories/virtual/GridStory.tsx
+++ packages/data/stories/virtual/GridStory.tsx
@@ -25,7 +25,8 @@ export const TempItem = (props: GridItemProps) => undefined;
 export const LIPSUM =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula nulla luctus sem congue iaculis. Praesent lobortis gravida nibh non sodales. Ut lacinia nulla ac pellentesque ullamcorper. Phasellus non quam lorem. Etiam consectetur dapibus dapibus. Nunc diam libero, facilisis a rhoncus eget, fermentum non lacus.";
 
-export const GridStory: ComponentStory<typeof AxGridView> = ({ count }) => {
+const count = 10;
+export const GridStory: ComponentStory<typeof AxGridView> = ({}) => {
   const [isLoading, setLoading] = useState(false);
   const [recordCount, setCount] = useState(0);
 
@@ -42,37 +43,39 @@ export const GridStory: ComponentStory<typeof AxGridView> = ({ count }) => {
   return (
     <AxViewport>
       <AxGridView
-        count={recordCount}
+        items={new Array(recordCount).fill(true)}
         onLoadMore={loadMore}
         isLoading={isLoading}
       >
-        {(props) => (
-          <AxGridView.Item {...props}>
-            <AxPanel isCollapsable>
-              <AxHeader>
-                <AxTitle>List item {props.index + 1}</AxTitle>
-              </AxHeader>
-              <AxContent>
-                <AxFlexBox>
-                  <AxFlexBox.Row>
-                    <AxFlexBox.Col flex="auto">
-                      <img
-                        height={96}
-                        width={128}
-                        loading="lazy"
-                        className="border-4 border-bw-500 object-contain"
-                        src={`https://picsum.photos/id/${props.index}/192/108`}
-                      />
-                    </AxFlexBox.Col>
-                    <AxFlexBox.Col flex="fill">
-                      <AxText clip={6}>{LIPSUM}</AxText>
-                    </AxFlexBox.Col>
-                  </AxFlexBox.Row>
-                </AxFlexBox>
-              </AxContent>
-            </AxPanel>
-          </AxGridView.Item>
-        )}
+        {(props) =>
+          props.data && (
+            <AxGridView.Item {...props}>
+              <AxPanel isCollapsable>
+                <AxHeader>
+                  <AxTitle>List item {props.index + 1}</AxTitle>
+                </AxHeader>
+                <AxContent>
+                  <AxFlexBox>
+                    <AxFlexBox.Row>
+                      <AxFlexBox.Col flex="auto">
+                        <img
+                          height={96}
+                          width={128}
+                          loading="lazy"
+                          className="border-4 border-bw-500 object-contain"
+                          src={`https://picsum.photos/id/${props.index}/192/108`}
+                        />
+                      </AxFlexBox.Col>
+                      <AxFlexBox.Col flex="fill">
+                        <AxText clip={6}>{LIPSUM}</AxText>
+                      </AxFlexBox.Col>
+                    </AxFlexBox.Row>
+                  </AxFlexBox>
+                </AxContent>
+              </AxPanel>
+            </AxGridView.Item>
+          )
+        }
       </AxGridView>
     </AxViewport>
   );
