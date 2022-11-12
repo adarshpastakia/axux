8592da90353edb9e12040b16fd1bc3b0acb639ac
Adarsh Pastakia <adarshpastakia@outlook.com>
2022-11-12 18:22:51 +0400

    fix(Thumbnail): fix load error handling

--- packages/media/src/thumbnail/Thumbnail.tsx
+++ packages/media/src/thumbnail/Thumbnail.tsx
@@ -6,11 +6,12 @@
  * @license   : MIT
  */
 
-import { AxAnimation } from "@axux/core";
+import { AxAnimation, AxIcon } from "@axux/core";
 import { ElementProps } from "@axux/core/dist/types";
 import { getImageColorset } from "@axux/utilities/dist/getImageColorset";
 import {
   FC,
+  MouseEvent,
   SyntheticEvent,
   useCallback,
   useEffect,
@@ -59,16 +60,29 @@ export const AxThumbnail: FC<ThumbnailProps> = ({
   const [src, setSrc] = useState<string>();
   const [_, startTransition] = useTransition();
   const [isLoading, setLoading] = useState(true);
+  const [isErrored, setErrored] = useState(0);
 
   useEffect(() => {
     setSrc("");
     startTransition(() => {
+      setErrored(0);
       setLoading(true);
       setSrc(_src);
     });
   }, [_src]);
 
-  useEffect(() => setLoading(true), [src]);
+  const reload = useCallback(
+    (e: MouseEvent) => {
+      e.stopPropagation();
+      startTransition(() => {
+        setErrored(0);
+        setLoading(true);
+        setSrc(_src);
+      });
+    },
+    [_src]
+  );
+
   const loadHandler = useCallback((e: SyntheticEvent<HTMLImageElement>) => {
     e.currentTarget.dataset.colorset = getImageColorset(e.currentTarget);
     startTransition(() => {
@@ -77,9 +91,15 @@ export const AxThumbnail: FC<ThumbnailProps> = ({
   }, []);
   const handleError = useCallback(() => {
     startTransition(() => {
-      setSrc(fallback);
+      if (isErrored === 1) {
+        setErrored(2);
+        setLoading(false);
+      } else {
+        setErrored(1);
+        setSrc(fallback);
+      }
     });
-  }, [fallback]);
+  }, [isErrored, fallback]);
 
   return (
     <div
@@ -88,13 +108,22 @@ export const AxThumbnail: FC<ThumbnailProps> = ({
       style={{ width, height }}
       className={`ax-thumbnail ${className ?? ""}`}
     >
-      <img
-        src={src}
-        loading="lazy"
-        crossOrigin="anonymous"
-        onLoad={loadHandler}
-        onError={src ? handleError : undefined}
-      />
+      {isErrored !== 2 && (
+        <img
+          src={src}
+          loading="lazy"
+          crossOrigin="anonymous"
+          onLoad={loadHandler}
+          onError={src ? handleError : undefined}
+        />
+      )}
+      {isErrored === 2 && (
+        <AxIcon
+          onClick={reload}
+          className="ax-thumbnail--reload"
+          icon="M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z"
+        />
+      )}
       {isLoading && (
         <div className="ax-thumbnail--spinner">
           <AxAnimation.Spinner />
