a70e7153d0e165eb2186223a0698abcd8eb7cf32
Adarsh Pastakia <adarshpastakia@outlook.com>
2022-11-14 18:03:24 +0400

    feat(MediaCanvas): added draw/hilight polygon

--- packages/media/src/canvas/Canvas.tsx
+++ packages/media/src/canvas/Canvas.tsx
@@ -11,6 +11,7 @@ import {
   FC,
   HTMLAttributes,
   memo,
+  Reducer,
   RefObject,
   useCallback,
   useEffect,
@@ -20,7 +21,8 @@ import {
 } from "react";
 
 interface BoxObject {
-  box: string | number[];
+  box?: string | number[];
+  polygon?: string[] | number[][];
   colorTop?: string;
   colorBottom?: string;
   labelTop?: string;
@@ -31,12 +33,17 @@ interface BoxObject {
 
 export interface CanvasRef {
   clear: () => void;
+  unhilight: () => void;
   drawBox: (
     boundingBox: BoxObject["box"],
-    options?: Omit<BoxObject, "box">
+    options?: Omit<BoxObject, "box" | "polygon">
   ) => void;
-  unhilight: () => void;
-  hilightBox: (boundingBox: string | number[]) => void;
+  drawPolygon: (
+    boundingBox: BoxObject["polygon"],
+    options?: Omit<BoxObject, "box" | "polygon">
+  ) => void;
+  hilightBox: (boundingBox: BoxObject["box"]) => void;
+  hilightPolygon: (boundingBox: BoxObject["polygon"]) => void;
 }
 
 export interface CanvasProps {
@@ -60,7 +67,7 @@ export const Canvas: FC<CanvasProps> = memo(({ style, media, canvas }) => {
     return ratio;
   }, [media, style]);
 
-  const hilightArea = useCallback((box: string | number[]) => {
+  const hilightArea = useCallback((box: BoxObject["box"]) => {
     const ratio = getRatio();
     const context = canvasRef.current?.getContext("2d");
     if (context) {
@@ -73,7 +80,31 @@ export const Canvas: FC<CanvasProps> = memo(({ style, media, canvas }) => {
     }
   }, []);
 
-  const [objects, dispatch] = useReducer(
+  const hilightPolygon = useCallback((polygon: BoxObject["polygon"]) => {
+    const ratio = getRatio();
+    const context = canvasRef.current?.getContext("2d");
+    if (context && polygon) {
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+      context.fillStyle = "#333333CC";
+      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+      context.globalCompositeOperation = "destination-out";
+      context.lineWidth = 1;
+      context.beginPath();
+      polygon.forEach((box, i) => {
+        const [x, y] = getBox(box);
+        i === 0 && context.moveTo(x * ratio, y * ratio);
+        i > 0 && context.lineTo(x * ratio, y * ratio);
+      });
+      context.closePath();
+      context.stroke();
+      context.fill();
+      context.globalCompositeOperation = "source-over";
+    }
+  }, []);
+
+  const [objects, dispatch] = useReducer<Reducer<BoxObject[], AnyObject>>(
     (state: BoxObject[], action: KeyValue) => {
       if (action.type === "clear") {
         return [];
@@ -86,6 +117,9 @@ export const Canvas: FC<CanvasProps> = memo(({ style, media, canvas }) => {
       }
       if (action.type === "hilight") {
         hilightArea(action.payload);
+      }
+      if (action.type === "hilightPolygon") {
+        hilightPolygon(action.payload);
       }
       return state;
     },
@@ -105,8 +139,14 @@ export const Canvas: FC<CanvasProps> = memo(({ style, media, canvas }) => {
         drawBox: (box, options = {}) => {
           dispatch({ type: "drawBox", payload: { box, ...options } });
         },
+        drawPolygon: (polygon, options = {}) => {
+          dispatch({ type: "drawBox", payload: { polygon, ...options } });
+        },
         hilightBox: (box) => {
           dispatch({ type: "hilight", payload: box });
+        },
+        hilightPolygon: (polygon) => {
+          dispatch({ type: "hilightPolygon", payload: polygon });
         },
       };
     },
@@ -118,11 +158,55 @@ export const Canvas: FC<CanvasProps> = memo(({ style, media, canvas }) => {
     context?.clearRect(0, 0, context.canvas.width, context.canvas.height);
     const ratio = getRatio();
     if (context) {
-      objects.forEach(({ box, ...options }) => {
-        const [x, y, w, h] = getBox(box);
-        context.lineWidth = 3;
-        context.strokeStyle = options?.stroke ?? "#fc0";
-        context.strokeRect(x * ratio, y * ratio, w * ratio, h * ratio);
+      objects.forEach(({ box, polygon, ...options }) => {
+        if (box) {
+          const [x, y, w, h] = getBox(box);
+          context.lineWidth = 3;
+          context.strokeStyle = options?.stroke ?? "#fc0";
+          context.strokeRect(x * ratio, y * ratio, w * ratio, h * ratio);
+          if (options.labelTop) {
+            const labelX = x * ratio;
+            let labelY = y * ratio - 18;
+            if (labelY < 0) labelY += 21;
+            context.font = "13px sans-serif";
+            context.fillStyle = options.colorTop ?? "#111111ee";
+            context.fillRect(labelX, labelY, w, 18);
+            context.globalCompositeOperation = "xor";
+            context.fillStyle = "#ffffff";
+            context.fillText(`${options.labelTop}`, labelX + 2, labelY + 15, w);
+          }
+          if (options.labelBottom) {
+            if (options.labelTop) {
+              const labelX = x * ratio;
+              let labelY = y * ratio + h * ratio + 3;
+              if (labelY > h) labelY -= 21;
+              context.font = "13px sans-serif";
+              context.fillStyle = options.colorBottom ?? "#111111ee";
+              context.fillRect(labelX, labelY, w, 18);
+              context.globalCompositeOperation = "xor";
+              context.fillStyle = "#ffffff";
+              context.fillText(
+                `${options.labelBottom}`,
+                labelX + 2,
+                labelY + 15,
+                w
+              );
+            }
+          }
+          context.globalCompositeOperation = "source-over";
+        }
+        if (polygon) {
+          context.lineWidth = 3;
+          context.strokeStyle = options?.stroke ?? "#fc0";
+          context.beginPath();
+          polygon.forEach((box, i) => {
+            const [x, y] = getBox(box);
+            i === 0 && context.moveTo(x * ratio, y * ratio);
+            i > 0 && context.lineTo(x * ratio, y * ratio);
+          });
+          context.closePath();
+          context.stroke();
+        }
       });
     }
   }, [objects, getRatio]);
