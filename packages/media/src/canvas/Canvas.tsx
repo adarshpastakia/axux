e23fa77871b5179561aa86c00ae7512af591ff1d
Adarsh Pastakia <adarshpastakia@outlook.com>
2022-11-18 11:26:30 +0400

    fix(MediaCanvas): fix drawBox/drawPolygon

--- packages/media/src/canvas/Canvas.tsx
+++ packages/media/src/canvas/Canvas.tsx
@@ -67,42 +67,62 @@ export const Canvas: FC<CanvasProps> = memo(({ style, media, canvas }) => {
     return ratio;
   }, [media, style]);
 
-  const hilightArea = useCallback((box: BoxObject["box"]) => {
-    const ratio = getRatio();
-    const context = canvasRef.current?.getContext("2d");
-    if (context) {
-      const [x, y, w, h] = getBox(box);
-      context?.clearRect(0, 0, context.canvas.width, context.canvas.height);
-      context.fillStyle = "rgb(0 0 0 / 0.5)";
-      context.globalCompositeOperation = "overlay";
-      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-      context.clearRect(x * ratio, y * ratio, w * ratio, h * ratio);
-    }
-  }, []);
+  const hilightArea = useCallback(
+    (box: BoxObject["box"], strokeColor?: string) => {
+      const ratio = getRatio();
+      const context = canvasRef.current?.getContext("2d");
+      if (context) {
+        const [x, y, w, h] = getBox(box);
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        context.fillStyle = "rgb(0 0 0 / 0.5)";
+        context.globalCompositeOperation = "overlay";
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        context.clearRect(x * ratio, y * ratio, w * ratio, h * ratio);
+        context.lineWidth = 1;
+        context.strokeStyle = strokeColor ?? "#fc0";
+        context.strokeRect(x * ratio, y * ratio, w * ratio, h * ratio);
+      }
+    },
+    []
+  );
 
-  const hilightPolygon = useCallback((polygon: BoxObject["polygon"]) => {
-    const ratio = getRatio();
-    const context = canvasRef.current?.getContext("2d");
-    if (context && polygon) {
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  const hilightPolygon = useCallback(
+    (polygon: BoxObject["polygon"], strokeColor?: string) => {
+      const ratio = getRatio();
+      const context = canvasRef.current?.getContext("2d");
+      if (context && polygon) {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
-      context.fillStyle = "#333333CC";
-      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        context.fillStyle = "#333333CC";
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
-      context.globalCompositeOperation = "destination-out";
-      context.lineWidth = 1;
-      context.beginPath();
-      polygon.forEach((box, i) => {
-        const [x, y] = getBox(box);
-        i === 0 && context.moveTo(x * ratio, y * ratio);
-        i > 0 && context.lineTo(x * ratio, y * ratio);
-      });
-      context.closePath();
-      context.stroke();
-      context.fill();
-      context.globalCompositeOperation = "source-over";
-    }
-  }, []);
+        context.globalCompositeOperation = "destination-out";
+        context.lineWidth = 1;
+        context.beginPath();
+        polygon.forEach((box, i) => {
+          const [x, y] = getBox(box);
+          i === 0 && context.moveTo(x * ratio, y * ratio);
+          i > 0 && context.lineTo(x * ratio, y * ratio);
+        });
+        context.closePath();
+        context.stroke();
+        context.fill();
+        context.globalCompositeOperation = "source-over";
+
+        context.lineWidth = 1;
+        context.strokeStyle = strokeColor ?? "#fc0";
+        context.beginPath();
+        polygon.forEach((box, i) => {
+          const [x, y] = getBox(box);
+          i === 0 && context.moveTo(x * ratio, y * ratio);
+          i > 0 && context.lineTo(x * ratio, y * ratio);
+        });
+        context.closePath();
+        context.stroke();
+      }
+    },
+    []
+  );
 
   const [objects, dispatch] = useReducer<Reducer<BoxObject[], AnyObject>>(
     (state: BoxObject[], action: KeyValue) => {
@@ -160,38 +180,46 @@ export const Canvas: FC<CanvasProps> = memo(({ style, media, canvas }) => {
     if (context) {
       objects.forEach(({ box, polygon, ...options }) => {
         if (box) {
-          const [x, y, w, h] = getBox(box);
-          context.lineWidth = 3;
+          let [x, y, w, h] = getBox(box);
+          x *= ratio;
+          y *= ratio;
+          w *= ratio;
+          h *= ratio;
+          context.lineWidth = 2;
           context.strokeStyle = options?.stroke ?? "#fc0";
-          context.strokeRect(x * ratio, y * ratio, w * ratio, h * ratio);
+          context.strokeRect(x, y, w, h);
           if (options.labelTop) {
-            const labelX = x * ratio;
-            let labelY = y * ratio - 18;
+            const labelX = x - 1;
+            let labelY = y - 18;
             if (labelY < 0) labelY += 21;
-            context.font = "13px sans-serif";
-            context.fillStyle = options.colorTop ?? "#111111ee";
-            context.fillRect(labelX, labelY, w, 18);
-            context.globalCompositeOperation = "xor";
+            context.fillStyle = options.colorTop ?? "rgb(0 0 0 / 0.5)";
+            context.fillRect(labelX, labelY, w + 2, 18);
             context.fillStyle = "#ffffff";
-            context.fillText(`${options.labelTop}`, labelX + 2, labelY + 15, w);
+            context.font = "13px sans-serif";
+            context.globalCompositeOperation = "screen";
+            context.fillText(
+              `${options.labelTop}`,
+              labelX + 2,
+              labelY + 15,
+              w - 4
+            );
           }
+          context.globalCompositeOperation = "source-over";
           if (options.labelBottom) {
-            if (options.labelTop) {
-              const labelX = x * ratio;
-              let labelY = y * ratio + h * ratio + 3;
-              if (labelY > h) labelY -= 21;
-              context.font = "13px sans-serif";
-              context.fillStyle = options.colorBottom ?? "#111111ee";
-              context.fillRect(labelX, labelY, w, 18);
-              context.globalCompositeOperation = "xor";
-              context.fillStyle = "#ffffff";
-              context.fillText(
-                `${options.labelBottom}`,
-                labelX + 2,
-                labelY + 15,
-                w
-              );
-            }
+            const labelX = x - 1;
+            let labelY = y + h;
+            if (labelY > context.canvas.height) labelY -= 21;
+            context.fillStyle = options.colorBottom ?? "rgb(0 0 0 / 0.5)";
+            context.fillRect(labelX, labelY, w + 2, 18);
+            context.fillStyle = "#ffffff";
+            context.font = "13px sans-serif";
+            context.globalCompositeOperation = "screen";
+            context.fillText(
+              `${options.labelBottom}`,
+              labelX + 2,
+              labelY + 15,
+              w - 4
+            );
           }
           context.globalCompositeOperation = "source-over";
         }
