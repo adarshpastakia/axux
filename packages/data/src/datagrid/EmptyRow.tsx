01f75098ec14e813df960325543e9ea94c1d8947
Adarsh Pastakia <adarshpastakia@outlook.com>
2022-12-17 00:00:41 +0400

    fix(AxDatagrid): disable pointer event on empty row

--- packages/data/src/datagrid/EmptyRow.tsx
+++ packages/data/src/datagrid/EmptyRow.tsx
@@ -25,7 +25,7 @@ export const EmptyRow = () => {
   );
 
   return (
-    <div className="ax-datagrid__row flex-1">
+    <div className="ax-datagrid__row flex-1 pointer-events-none">
       <div className="ax-datagrid__row--flex min-h-full">
         <div className="ax-datagrid__fixStart">
           {!(onRowExpand == null) && (
