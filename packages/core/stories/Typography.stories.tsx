5463cec57164a64286be025a3c30b16b22efe872
Adarsh Pastakia <adarshpastakia@outlook.com>
2023-08-20 00:27:43 +0400

    docs: add formatters

--- packages/core/stories/Typography.stories.tsx
+++ packages/core/stories/Typography.stories.tsx
@@ -1,6 +1,7 @@
 import { faker } from "@faker-js/faker";
 import type { Meta, StoryObj } from "@storybook/react";
 import { AxText } from "../src";
+import { Format } from "@axux/utilities";
 
 const meta: Meta<typeof AxText> = {
   title: "@core/Typography",
@@ -71,5 +72,45 @@ export const Copy: StoryObj<typeof AxText.Copy> = {
     Dolores officiis ut repudiandae officiis.
     Perferendis nulla sequi quibusdam maiores porro modi doloremque doloribus illo.
     Similique nobis ratione eius dolores et.`,
+  },
+};
+
+export const Formatters: StoryObj<{
+  phone: string;
+  bytes: number;
+  number: number;
+  numberFormat: string;
+  seconds: number;
+}> = {
+  render: (args) => (
+    <div className="w-[30rem] p-8">
+      <AxText>Phone: {Format.phone(args.phone, true)}</AxText>
+      <code className="inline-block bg-base text-muted mb-4 text-sm">
+        Format.phone(phoneNumber)
+      </code>
+      <AxText>Bytes: {Format.bytes(args.bytes)}</AxText>
+      <code className="inline-block bg-base text-muted mb-4 text-sm">
+        Format.bytes(bytes)
+      </code>
+      <AxText>Number: {Format.number(args.number, args.numberFormat)}</AxText>
+      <code className="inline-block bg-base text-muted mb-4 text-sm">
+        Format.number(number, format)
+      </code>
+      <AxText>Time: {Format.duration(args.seconds, true)}</AxText>
+      <code className="inline-block bg-base text-muted mb-4 text-sm">
+        Format.duration(seconds, isFraction)
+      </code>
+      <AxText>Time: {Format.durationSeconds(args.seconds, true)}</AxText>
+      <code className="inline-block bg-base text-muted mb-4 text-sm">
+        Format.durationSeconds(seconds, isFraction)
+      </code>
+    </div>
+  ),
+  args: {
+    phone: "+971501234567",
+    bytes: 1512,
+    number: 12489.99,
+    numberFormat: "0,0.00",
+    seconds: 12489.128,
   },
 };
