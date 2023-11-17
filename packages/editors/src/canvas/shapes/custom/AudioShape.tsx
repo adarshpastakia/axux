34df7e91c996136105f38ee825db2de124266358
Adarsh Pastakia <adarshpastakia@outlook.com>
2023-11-18 00:59:23 +0400

    fix(Canvas): export to svg for shapes

--- packages/editors/src/canvas/shapes/custom/AudioShape.tsx
+++ packages/editors/src/canvas/shapes/custom/AudioShape.tsx
@@ -13,7 +13,8 @@ import {
   ShapeUtil,
   type TLBaseShape,
 } from "@tldraw/tldraw";
-import { useRef, useEffect } from "react";
+import domtoimage from "dom-to-image";
+import { useEffect, useRef } from "react";
 
 type AudioShape = TLBaseShape<
   "audio-card",
@@ -26,6 +27,7 @@ type AudioShape = TLBaseShape<
 
 export class AudioShapeUtil extends ShapeUtil<AudioShape> {
   static override type = "audio-card" as const;
+  containerRef?: HTMLElement;
 
   canResize = () => false;
 
@@ -61,19 +63,21 @@ export class AudioShapeUtil extends ShapeUtil<AudioShape> {
     }, []);
 
     return (
-      <HTMLContainer
-        id={shape.id}
-        className="bg-zinc-900 relative p-1 flex flex-col"
-      >
-        <audio
-          controls
-          ref={audioRef}
-          src={shape.props.src}
-          className="object-contain flex-1 w-full pointer-events-auto overflow-hidden"
-        />
-        <div className="bg-zinc-800/80 text-white items-center py-1 px-2 flex gap-1 text-sm">
-          <div className="truncate flex-1">{shape.props.fileName}</div>
-          <div>{Format.bytes(shape.props.fileSize ?? 0)}</div>
+      <HTMLContainer className="relative grid">
+        <div
+          ref={(el: HTMLDivElement) => (this.containerRef = el)}
+          className="bg-zinc-900 relative p-1 flex flex-col"
+        >
+          <audio
+            controls
+            ref={audioRef}
+            src={shape.props.src}
+            className="object-contain flex-1 w-full pointer-events-auto overflow-hidden"
+          />
+          <div className="bg-zinc-800/80 text-white items-center py-1 px-2 flex gap-1 text-sm">
+            <div className="truncate flex-1">{shape.props.fileName}</div>
+            <div>{Format.bytes(shape.props.fileSize ?? 0)}</div>
+          </div>
         </div>
       </HTMLContainer>
     );
@@ -81,5 +85,18 @@ export class AudioShapeUtil extends ShapeUtil<AudioShape> {
 
   indicator(shape: AudioShape) {
     return <rect />;
+  }
+
+  async toSvg() {
+    if (!this.containerRef)
+      return document.createElementNS("http://www.w3.org/2000/svg", "image");
+    return await domtoimage.toPng(this.containerRef).then(function (dataUrl) {
+      const image = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "image"
+      );
+      image.setAttributeNS("http://www.w3.org/1999/xlink", "href", dataUrl);
+      return image;
+    });
   }
 }
