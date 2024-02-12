0aa6542c22f9099054b59b45a54e6c920894d2cb
Adarsh Pastakia <adarshpastakia@outlook.com>
2024-02-12 13:29:16 +0400

    WIP: graph component updates

--- packages/data/src/graph/context/GraphContext.tsx
+++ packages/data/src/graph/context/GraphContext.tsx
@@ -11,24 +11,45 @@ import { Toolbar } from "../components/Toolbar";
 import { useGraph } from "../hooks/useGraph";
 import { type GraphProps } from "../types";
 
-const GraphContext = createContext({});
+const GraphContext = createContext<{ graph: ReturnType<typeof useGraph> }>(
+  {} as AnyObject
+);
 
-export const GraphProvider: FC<GraphProps> = ({ data }) => {
+export const GraphProvider: FC<GraphProps> = ({
+  data,
+  colorMap,
+  defaultLayout,
+  allowInteraction,
+  onNodeExpand,
+}) => {
   const [container, setContainer] = useState<HTMLDivElement | null>(null);
   const graph = useGraph(container);
 
   useEffect(() => {
-    if (graph) {
-      console.log("render graph");
-      graph.loadData(data as AnyObject);
-    }
-  }, [graph, data]);
+    graph.restyle(colorMap);
+  }, [graph.restyle, colorMap]);
+
+  useEffect(() => {
+    graph.resetLayout(defaultLayout);
+  }, [graph.resetLayout, defaultLayout]);
+
+  useEffect(() => {
+    graph.ref?.setMode(allowInteraction ? "edit" : "default");
+  }, [graph.ref, allowInteraction]);
+
+  useEffect(() => {
+    console.log("render graph");
+    graph.loadData(data as AnyObject);
+  }, [graph.loadData, data]);
 
   return (
-    <GraphContext.Provider value={{}}>
+    <GraphContext.Provider value={{ graph }}>
       <div className="ax-graph-viewer">
         <div className="ax-graph__container" ref={setContainer} />
-        <Toolbar />
+        <Toolbar
+          allowInteraction={allowInteraction}
+          onNodeExpand={onNodeExpand}
+        />
       </div>
     </GraphContext.Provider>
   );
