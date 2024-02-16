f5253872a3686947c185fd94d676b202d19bba31
Adarsh Pastakia <adarshpastakia@outlook.com>
2024-02-17 02:15:48 +0400

    WIP: export graphRef type

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -6,7 +6,10 @@
  * @license   : MIT
  */
 
+import { type GraphProps } from "./graph/types";
 import "./i18n";
+
+type GraphRef = GraphProps["graphRef"];
 
 export { AxCheckList } from "./checklist/CheckList";
 export { AxDatagrid } from "./datagrid/Datagrid";
@@ -27,3 +30,4 @@ export { AxVirtualList } from "./virtual/List";
 export type { VirtualListRef } from "./virtual/List";
 
 export { AxGraph } from "./graph/Graph";
+export { type GraphRef };
